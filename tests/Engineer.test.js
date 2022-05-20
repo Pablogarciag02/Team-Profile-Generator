defb3c444fb8ba616574c6fe340a2154bd92466f
Pablo Garcia <pgarciag566@gmail.com>
2022-05-20 07:32:18 -0500

    now asks engineer for github link. (works)

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,19 +1,25 @@
 const Engineer = require("../lib/engineer");
 
 test ("Engineer object is created", () => {
-    const engineer = new Engineer ("Pablo", "2", "pgarciag566@gmail.com", "Pablogarciag02")
+    const engineer = new Engineer ("Pablo", "2", "pgarciag566@gmail.com", "Pablogarciag02", "https://github.com/Pablogarciag02")
 
     expect(engineer.github).toEqual(expect.any(String));
 })
 
 test ("getGithub() will return this.github", () => {
-    const engineer = new Engineer ("Pablo", "2", "pgarciag566@gmail.com", "Pablogarciag02")
+    const engineer = new Engineer ("Pablo", "2", "pgarciag566@gmail.com", "Pablogarciag02", "https://github.com/Pablogarciag02")
 
     expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()))
 })
 
+test ("getgithubLink() will return this.githubLink", () => {
+    const engineer = new Engineer ("Pablo", "2", "pgarciag566@gmail.com", "Pablogarciag02", "https://github.com/Pablogarciag02")
+
+    expect(engineer.getgithubLink()).toEqual(expect.stringContaining(engineer.githubLink.toString()))
+})
+
 test ("GetRole() returns === Engineer ", () => {
-    const engineer = new Engineer ("Pablo", 1, "pgarciag566@gmail.com", "Pablogarciag02")
+    const engineer = new Engineer ("Pablo", 1, "pgarciag566@gmail.com", "Pablogarciag02", "https://github.com/Pablogarciag02")
 
     expect(engineer.getRole()).toEqual("Engineer");
 });
